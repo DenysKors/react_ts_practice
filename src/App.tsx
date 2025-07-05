a4561cb2452bb5e16bc971b97b5d4779b5c5c2d2
Dan <105918404+DenysKors@users.noreply.github.com>
2025-07-05 13:49:05 +0300

    Add getDetail function

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,60 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <h1>Typescript practice</h1>;
 }
 
 export default App;
 
-type PizzaType = {
+type Pizza = {
+  id: number;
   name: string;
   price: number;
 };
 
-type PizzaOrderType = {
+type PizzaOrder = {
   id: number;
-  pizza: PizzaType;
+  pizza: Pizza;
   status: "ordered" | "completed";
 };
 
-const menu: PizzaType[] = [
+let cashInRegister = 100;
+let newPizzaId = 0;
+let newOrderId = 0;
+const orderQueue: PizzaOrder[] = [];
+
+const menu: Pizza[] = [
   { name: "Margherita", price: 8 },
   { name: "Pepperoni", price: 10 },
   { name: "Hawaiian", price: 10 },
   { name: "Veggie", price: 9 },
-];
+].map((pizza) => {
+  newPizzaId += 1;
+  return { ...pizza, id: newPizzaId };
+});
 
-let cashInRegister = 100;
-let newOrderId = 0;
-const orderQueue: PizzaOrderType[] = [];
+// const menu: Pizza[] = [
+//   { id: (newPizzaId += 1), name: "Margherita", price: 8 },
+//   { id: (newPizzaId += 1), name: "Pepperoni", price: 10 },
+//   { id: (newPizzaId += 1), name: "Hawaiian", price: 10 },
+//   { id: (newPizzaId += 1), name: "Veggie", price: 9 },
+// ];
 
 function addNewPizza(name: string, price: number): void {
-  const newPizza = { name, price };
+  newPizzaId += 1;
+  const newPizza = { name, price, id: newPizzaId };
   menu.push(newPizza);
   console.log(menu);
 }
 
-function placeOrder(pizzaName: string): PizzaType | void {
+function placeOrder(pizzaName: string): Pizza | void {
   const orderedPizza = menu.find((pizza) => pizza.name === pizzaName);
 
   if (!orderedPizza) return alert(`${pizzaName}  not found`);
 
   cashInRegister = cashInRegister + orderedPizza.price;
   newOrderId += 1;
-  const newOrder: PizzaOrderType = {
+  const newOrder: PizzaOrder = {
     id: newOrderId,
     pizza: orderedPizza,
     status: "ordered",
@@ -80,7 +65,7 @@ function placeOrder(pizzaName: string): PizzaType | void {
   return orderedPizza;
 }
 
-function completeOrder(orderId: number): PizzaOrderType | void {
+function completeOrder(orderId: number): PizzaOrder | void {
   const orderIdx = orderQueue.findIndex((order) => order.id === orderId);
   if (orderIdx === -1) return alert(`Order ${orderId} not found`);
 
@@ -89,10 +74,24 @@ function completeOrder(orderId: number): PizzaOrderType | void {
   return orderQueue[orderIdx];
 }
 
+function getPizzaDetail(identifier: string | number): Pizza | undefined | void {
+  if (typeof identifier === "string") {
+    const pizzaFromMenu = menu.find(
+      (pizza) => pizza.name.toLowerCase() === identifier.toLowerCase()
+    );
+    return pizzaFromMenu;
+  } else if (typeof identifier === "number") {
+    const pizzaFromMenu = menu.find((pizza) => pizza.id === identifier);
+    return pizzaFromMenu;
+  } else return alert("You can find pizza only by name or id");
+}
+
 addNewPizza("Four cheeses", 15);
 placeOrder("Pepperoni");
 placeOrder("Hawaiian");
 placeOrder("Margherita");
 completeOrder(2);
+console.log(getPizzaDetail(3));
+console.log(getPizzaDetail("Veggie"));
 console.log(orderQueue);
 console.log(cashInRegister);
