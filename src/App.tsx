d7bcc12813bc8552affdbd251c741f02b5ec0fe7
Dan <105918404+DenysKors@users.noreply.github.com>
2025-07-25 20:21:37 +0300

    Sort words kata

--- src/App.tsx
+++ src/App.tsx
@@ -133,6 +133,7 @@ console.log(
 // KATA #2
 /* 
 Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
 Example createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 */
 
@@ -154,11 +155,11 @@ console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 // KATA #3
 /*
 In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
-Kata.HighAndLow("1 2 3 4 5"); // return "5 1"
-
 All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
+
+Kata.HighAndLow("1 2 3 4 5"); // return "5 1"
 */
 
 function highAndLowNumber(numbers: string): string {
@@ -184,6 +185,7 @@ console.log(highAndLowNumber("1"));
 Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct.
 However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 Your task is to write a function maskify, which changes all but the last four characters into '*'.
+
 "4556364607935616"-- > "*************5616";
 "64607935616"-- > "*********5616";
 "1"-- > "1";
@@ -208,6 +210,7 @@ console.log(maskify("1"));
 Complete the method / function so that it converts dash / underscore delimited words into camel casing.
 The first word within the output should be capitalized only if the original word was capitalized(known as Upper Camel Case, also often referred to as Pascal case).
 The next words should be always capitalized.
+
 "the-stealth-warrior" gets converted to "theStealthWarrior"
 "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
 "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
@@ -234,6 +237,7 @@ console.log(camelCaseConverter("The_Stealth-Warrior"));
 /*
 Create a function nextVersion, that will take a string in parameter, and will return a string containing the next version number.
 All numbers, except the first one, must be lower than 10: if there are, you have to set them to 0 and increment the next number in sequence.
+
 Current           ->  Next version
 "1.2.3"           ->  "1.2.4"
 "0.9.9"           ->  "1.0.0"
@@ -282,6 +286,7 @@ console.log(nextVersion("10.1.5.9.9"));
 /*
 Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
 which is the number of times you must multiply the digits in num until you reach a single digit.
+
 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit, there are 3 multiplications)
 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2, there are 4 multiplications)
 4 --> 0 (because 4 is already a one-digit number, there is no multiplication)
@@ -305,3 +310,34 @@ function persistence(num: number): string | number {
 }
 
 console.log(persistence(999));
+
+// KATA #8
+/*
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+
+function sortWords(sentence: string): string | string[] | null {
+  if (sentence.length === 0) return "";
+
+  const re = /[1-9]/g;
+  const foundNumArr = sentence.match(re);
+  const stringArr = sentence.split(" ");
+  const sortedArr = [];
+
+  for (let i = 1; i <= stringArr.length; i += 1) {
+    const match = foundNumArr?.indexOf(i.toString());
+    if (typeof match !== "undefined") sortedArr.push(stringArr[match]);
+  }
+  const sortedSentence = sortedArr.join(" ");
+
+  return sortedSentence;
+}
+
+console.log(sortWords("is2 Thi1s T4est 3a"));
+console.log(sortWords("4of Fo1r pe6ople g3ood th5e the2"));
